fa13c4b21ad850b464caf60c1074534a79cbd5d2
MtheMartian <marvin.altidor@outlook.com>
2022-11-02 22:50:52 -0400

    Peter the Baker

--- pete_the_baker.js
+++ pete_the_baker.js
@@ -2,16 +2,30 @@ function cakes(recipe, available){
   let numOfElements = 0;
   let [ingredient, amount] = Object.entries(recipe)[numOfElements];
   let howMany = 0;
-  for(const[key, value] of Object.entries(available)){
-    for(let i = 0; i < Object.keys(available).length; i++){
-      if(Object.keys(available)[i] === ingredient){
-        numOfElements = numOfElements + 1;
-        if(Object.values(available)[i] / amount >= 1){
-          howMany += (Object.values(available)[i] / amount);
-        }
-        console.log(key);
-      } 
+
+  const filteredAvailable = Object.keys(available).filter((ingredients, index) =>{
+    if(Object.keys(recipe).includes(ingredients)){
+      return true;
+    }
+    else{
+      return false;
+    }
+  });
+
+  console.log(filteredAvailable);
+
+  if(filteredAvailable.length === Object.keys(recipe).length){
+    for(const[key, value] of Object.entries(available)){
+      for(let i = 0; i < Object.keys(available).length; i++){
+        if(Object.keys(available)[i] === ingredient){
+          numOfElements = numOfElements + 1;
+          if(Object.values(available)[i] / amount >= 1){
+            howMany += (Math.floor(Object.values(available)[i] / amount));
+          }
+          console.log(key);
+        } 
+      }
     }
   }
-  console.log(howMany / Object.keys(recipe).length);
+  console.log(Math.floor(howMany / Object.keys(recipe).length));
 }
