c18e65f09148be4fb80469bb13ff3fdb7f5ca43d
MtheMartian <marvin.altidor@outlook.com>
2022-11-03 22:52:43 -0400

    Pete the Baker

--- pete_the_baker.js
+++ pete_the_baker.js
@@ -2,6 +2,7 @@ function cakes(recipe, available){
   let numOfElements = 0;
   let [ingredient, amount] = Object.entries(recipe)[numOfElements];
   let howMany = 0;
+  let howManyTimes = 0;
 
   const filteredAvailable = Object.keys(available).filter((ingredients, index) =>{
     if(Object.keys(recipe).includes(ingredients)){
@@ -12,9 +13,21 @@ function cakes(recipe, available){
     }
   });
 
+  for(let i = 0; i < filteredAvailable.length; i ++){
+    if(filteredAvailable[i] === ingredient){
+      numOfElements += 1;
+      howManyTimes += 1;
+    }
+    else if(howManyTimes > 1){
+      return console.log("'Can't bake that cake!");
+    }
+  }
+
+
   console.log(filteredAvailable);
 
   if(filteredAvailable.length === Object.keys(recipe).length){
+    numOfElements = 0;
     for(const[key, value] of Object.entries(available)){
       for(let i = 0; i < Object.keys(available).length; i++){
         if(Object.keys(available)[i] === ingredient){
