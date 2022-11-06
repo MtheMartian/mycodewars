3d19c863549a54ac33ca8cbe5121717e3e27a892
MtheMartian <marvin.altidor@outlook.com>
2022-11-05 21:33:47 -0400

    Pete the Baker

--- pete_the_baker.js
+++ pete_the_baker.js
@@ -2,15 +2,8 @@ function cakes(recipe, available){
   let numOfElements = 0;
   let [ingredient, amount] = Object.entries(recipe)[numOfElements];
   let availableAmount = 0;
-  let recipeAmount = 0;
   let howManyTimes = 0;
   let duplicate = [];
-
-  Object.values(recipe).forEach(amount =>{
-    recipeAmount += Number(amount);
-  });
-
-  console.log(recipeAmount);
 
   const filteredAvailable = Object.keys(available).filter((ingredients, index) =>{
     if(Object.keys(recipe).includes(ingredients)){
@@ -20,6 +13,9 @@ function cakes(recipe, available){
       return false;
     }
   });
+
+  const poopy = new Object(filteredAvailable);
+  console.log(poopy);
 
   for(let i = 0; i < filteredAvailable.length; i++){ 
     if(howManyTimes >= 1){
@@ -43,13 +39,18 @@ function cakes(recipe, available){
   console.log(filteredAvailable);
 
   if(filteredAvailable.length === Object.keys(recipe).length){
+    let numberAdded = false;
     numOfElements = 0;
     for(const[key, value] of Object.entries(available)){
       for(let i = 0; i < Object.keys(available).length; i++){
         if(Object.keys(available)[i] === ingredient){
           numOfElements = numOfElements + 1;
-          if(Object.values(available)[i] / amount >= 1){
-            availableAmount += Object.values(available)[i];
+          if(Object.values(available)[i] / amount >= 1 && numberAdded === false){
+            availableAmount = Math.floor(Object.values(available)[i]);
+            numberAdded = true;
+          }
+          if((Object.values(available)[i] / amount) < availableAmount){
+            availableAmount = Math.floor(Object.values(available)[i] / amount);
           }
           console.log(key);
         } 
@@ -57,5 +58,5 @@ function cakes(recipe, available){
     }
   }
   
-  Math.floor(availableAmount / recipeAmount) >= 1 ? console.log(Math.floor(availableAmount / recipeAmount)) : console.log(0);
+  console.log(availableAmount);
 }
